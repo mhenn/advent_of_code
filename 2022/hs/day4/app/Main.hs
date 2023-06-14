e4c31457d0fe82fb140a4ed96d08e07fb7c37e9c
mhenn <Mikeianhenning93@gmail.com>
2023-06-14 18:27:52 +0200

    finished day 4

--- 2022/hs/day4/app/Main.hs
+++ 2022/hs/day4/app/Main.hs
@@ -1,14 +1,29 @@
 module Main where
 import Data.List.Split (splitOn)
 
-parse :: String -> [[String]]
-parse =  map(concatMap (splitOn "-")) .map (splitOn ",") . lines
+parse :: String -> [[Int]]
+parse =  map(map read .concatMap (splitOn "-")) .map (splitOn ",") . lines
 
-inRange :: [String] -> Bool
-inRange [l1,r1,l2,r2] = True --TODO
+inRange :: [Int] -> Bool
+inRange [l1,r1,l2,r2] =  minimum [r1,r2] >= maximum [l1,l2] 
 inRange _ = False
+
+rangeContains:: [Int] -> Bool 
+rangeContains [l1,r1,l2,r2] =  l1 <= l2 && r1 >= r2
+rangeContains _ = False
+
+rangeEncapsulates :: [Int] -> Bool
+rangeEncapsulates [l1,r1,l2,r2] =  rangeContains [l1,r1,l2,r2] || rangeContains [l2,r2,l1,r1] 
+rangeEncapsulates _ = False
+
+taskA :: [[Int]] -> Int
+taskA =   length . filter id . map rangeEncapsulates 
+
+taskB :: [[Int]] -> Int
+taskB =   length . filter id . map inRange
 
 main :: IO ()
 main = do
-  content <- readFile "./res/test.txt"
-  print $ parse content
+  content <- readFile "./res/input.txt"
+  print $  taskA . parse $ content
+  print $  taskB . parse $ content
