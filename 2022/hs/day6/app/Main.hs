867cd6f2e36ba456e2b5c6ca48957c2749ad2273
mhenn <Mikeianhenning93@gmail.com>
2023-06-18 21:48:43 +0200

    finished day 6

--- 2022/hs/day6/app/Main.hs
+++ 2022/hs/day6/app/Main.hs
@@ -1,4 +1,17 @@
 module Main where
+import qualified Data.Set as Set
+
+sliding :: [Char] -> Int -> Int -> Int
+sliding xs size i 
+        | Set.size (Set.fromList cmp) /= size  = sliding newSlide size (i + 1)
+        | otherwise = i
+        where newSlide = tail xs
+              cmp  = take size xs
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main =  do 
+      content <- readFile "./res/input.txt" 
+      print $ sliding content a a
+      print $ sliding content b b
+      where a = 4 
+            b = 14
