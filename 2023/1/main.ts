58241fac91c5d13770b2a17cbeadd7068d578536
mhenn <Mikeianhenning93@gmail.com>
2023-12-02 19:16:04 +0100

    finished advent day2a

--- 2023/1/main.ts
+++ 2023/1/main.ts
@@ -1,28 +1,46 @@
-const input = Deno.readTextFileSync('./test2.txt');
+const input = Deno.readTextFileSync("./input2.txt");
 
-const in_arr = input.split('\n').filter(i => i);
+const in_arr = input.split("\n").filter((i) => i);
 
-const worded = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-
-
-function testForNumber(input:string): boolean{
-  if(parseInt(input)){
+function testForNumber(input: string): boolean {
+  if (parseInt(input)) {
     return true;
   }
   return false;
 }
 
+function slidingWindow(content: string, window: number): string {
+  let n = "";
+  for (let i = 0; i < content.length; i++) {
+    n += replaceItBadly(content.slice(i, i + window));
+  }
+  return n;
+}
 
+function replaceItBadly(content: string) {
+  return content
+    .replace(/one/g, "1")
+    .replace(/two/g, "2")
+    .replace(/three/g, "3")
+    .replace(/four/g, "4")
+    .replace(/five/g, "5")
+    .replace(/six/g, "6")
+    .replace(/seven/g, "7")
+    .replace(/eight/g, "8")
+    .replace(/nine/g, "9");
+}
 
+const sum = in_arr.reduce((acc, line) => {
+  console.log(line);
+  let numbers = slidingWindow(line, 5);
+  console.log(numbers);
 
-
-const sum = in_arr.reduce((acc,line) => {
-  console.log(line)
-  const numbers = line.split('').filter(inp => testForNumber(inp)).join('')
-  const num = numbers[0] + numbers.slice(-1)[0] 
+  numbers = numbers
+    .split("")
+    .filter((inp) => testForNumber(inp))
+    .join("");
+  const num = numbers[0] + numbers.slice(-1)[0];
   return acc + parseInt(num);
-
 }, 0);
 
-
-console.log(sum)
+console.log(sum);
---
+++ 2023/2/main.ts
@@ -0,0 +1,83 @@
+const input = Deno.readTextFileSync("./test.txt");
+
+function stringToRounds(rounds: string[]): Round[] {
+  return rounds.map((e) => new Round(e.split(",")));
+}
+
+class ColorAmount {
+  green: number;
+  blue: number;
+  red: number;
+
+  constructor(r: number = 0, g: number = 0, b: number = 0) {
+    this.red = r;
+    this.green = g;
+    this.blue = b;
+  }
+}
+
+class Game {
+  id: number;
+  rounds: Round[];
+  constructor(id: number, rounds: string[]) {
+    this.id = id;
+    this.rounds = stringToRounds(rounds);
+  }
+
+  containsColorAmount(colorAmount: ColorAmount) {
+    return (
+      this.rounds.filter((r) => r.contains(colorAmount)).length ===
+      this.rounds.length
+    );
+  }
+
+  smallestPossible() {
+    return this.rounds.reduce((acc: ColorAmount, curr: Round) => {},
+    new ColorAmount());
+  }
+}
+
+class Round {
+  boxes: { [color: string]: number };
+
+  constructor(boxes: string[]) {
+    this.boxes = {};
+    boxes.forEach((box) => {
+      const [_, amount, color] = box.split(" ");
+      this.boxes[color] = parseInt(amount);
+    });
+  }
+
+  contains(colorAmount: ColorAmount) {
+    const { green, red, blue } = colorAmount;
+    let ret = true;
+    const r = this.boxes["red"];
+    const g = this.boxes["green"];
+    const b = this.boxes["blue"];
+    if (r) ret = ret && r <= red;
+    if (g) ret = ret && g <= green;
+    if (b) ret = ret && b <= blue;
+    return ret;
+  }
+}
+
+const games = input
+  .split("\n")
+  .map((el) => el.split(":")[1] || "")
+  .filter((e) => e !== "")
+  .map((el, i) => new Game(i + 1, el.split(";")));
+
+function taskA(games: Game[]) {
+  const colorAmount = new ColorAmount(12, 13, 14);
+  return games.reduce((acc, curr) => {
+    if (curr.containsColorAmount(colorAmount)) return acc + curr.id;
+    return acc;
+  }, 0);
+}
+
+function taskB(games: Game[]) {
+  return games.reduce((acc, curr) => {}, 0);
+}
+
+const ret = taskA(games);
+console.log(ret);
